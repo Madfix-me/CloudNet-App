a455cd49d10928558681b92873fd6ff52c3ec999
Phillipp Glanz <p.glanz@madfix.me>
2022-01-20 17:49:22 +0100

    Clean up

--- lib/feature/dashboard/dashboard_page.dart
+++ lib/feature/dashboard/dashboard_page.dart
@@ -2,13 +2,9 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:async_redux/async_redux.dart';
-import 'package:cloudnet/feature/node/node_handler.dart';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
-import '/apis/cloudnetv3spec/model/node_info.dart';
 import '/state/actions/app_actions.dart';
 import '/state/app_state.dart';
-import 'package:cloudnet/i18n/strings.g.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -39,47 +35,21 @@ class _DashboardPageState extends State<DashboardPage> {
       },
       converter: (store) => store.state,
       builder: (context, state) {
-        print(state.token);
         return RefreshIndicator(
           onRefresh: _pullRefresh,
           child: Flex(
             direction: Axis.vertical,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.edit))
-                ],
-              ),
-              FutureBuilder(
-                future: WebSocket.connect(nodeHandler.currentWebsocketUrl() + '/api/v2/node/liveConsole',
-                    headers: <String, dynamic>{
-                      'Authorization': 'Bearer ${state.token}',
-                    }),
-                builder: (BuildContext context, AsyncSnapshot<WebSocket> snapshot) {
-                  print(snapshot.error);
-                  print(snapshot.hasData);
-                  return Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: StreamBuilder<String>(
-                        stream: snapshot.data?.cast(),
-                        builder: (context, ss) {
-                          return Text(ss.hasData ? '${ss.data}' : 'TEST');
-                        },
-                      ),
-                    ),
-                  );
-                },
-              )
-
-            ],
+            children: const [],
           ),
         );
       },
     );
   }
 
+  Future<void> _pullRefresh() async {
+    StoreProvider.dispatch(context, UpdateNodeInfoAction());
+  }
+/*
   Card buildCard(int index, NodeInfo info) {
     switch (index) {
       case 0:
@@ -206,5 +176,5 @@ class _DashboardPageState extends State<DashboardPage> {
         ),
       ),
     );
-  }
+  }*/
 }
