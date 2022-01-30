c3f426844e926a3781bf87eefcdce96543dc88d2
Phillipp Glanz <p.glanz@madfix.me>
2022-01-30 15:55:43 +0100

    Add new models, reformat code and cleanup code

--- lib/apis/cloudnetv3spec/model/cloudnet/cloud_player.dart
+++ lib/apis/cloudnetv3spec/model/cloudnet/cloud_player.dart
@@ -1,3 +1,4 @@
+import 'package:cloudnet/apis/cloudnetv3spec/model/cloudnet/network_player_proxy_info.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'cloud_player.freezed.dart';
@@ -6,7 +7,14 @@ part 'cloud_player.g.dart';
 
 @freezed
 class CloudPlayer with _$CloudPlayer {
-  const factory CloudPlayer() = _CloudPlayer;
+  const factory CloudPlayer({
+    @JsonKey(name: 'properties') @Default(<String, dynamic>{}) Map<String, dynamic> properties,
+    @JsonKey(name: 'name') String? name,
+    @JsonKey(name: 'firstLoginTimeMillis') BigInt? firstLoginTimeMillis,
+    @JsonKey(name: 'lastLoginTimeMillis') BigInt? lastLoginTimeMillis,
+    @JsonKey(name: 'lastNetworkPlayerProxyInfo') NetworkPlayerProxyInfo? lastNetworkPlayerProxyInfo,
+
+}) = _CloudPlayer;
 
   factory CloudPlayer.fromJson(Map<String, dynamic> json) =>
       _$CloudPlayerFromJson(json);
